8f14ebe57497e22a257ae217cf123cc277651c42
clba4780 <clba4780@colorado.edu>
2026-03-26 11:16:46 -0600

    creating plot

--- lab_07.py
+++ lab_07.py
@@ -8,5 +8,23 @@ import matplotlib.pyplot as plt
 # Load our CAMulator sample dataset
 ds = xr.open_dataset('sample_data_camulator.nc')
 
-coordinates = ds['TREHFHT'].sel(latitude=slice(35,72), longitude=slice(-25,65))
 
+fig, ax = plt.subplots(
+    figsize = (10,5),
+    subplot_kw= {'projection': ccrs.Robinson()}
+)
+# 2. Choose a good sequential colormap for precipitation (hint: 'GnBu' or 'Blues')
+# 3. Place the colorbar horizontally below the map
+ds['TREFHT'].isel(time=0, latitude=slice(35,72), longitude=slice(25,65)).plot(
+    ax = ax,
+    transform=ccrs.PlateCarree(),
+    cmap='GnBu', 
+    cbar_kwargs={'label': 'TREFHT (K)', 
+                 'shrink': 0.7, 
+                 'orientation': 'horizontal', 
+                 'pad': 0.05}
+)
+# 4. Add coastlines and gridlines
+ax.coastlines(linewidth=0.8)
+ax.gridlines(draw_labels=True)
+plt.show()
