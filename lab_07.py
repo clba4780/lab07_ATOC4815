43dcd7c10da3e50927ed97c68122d68ac0d7b535
clba4780 <clba4780@colorado.edu>
2026-03-29 12:26:14 -0600

    exploring data set and selecting region

--- lab_07.py
+++ lab_07.py
@@ -5,26 +5,15 @@ import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import matplotlib.pyplot as plt
 
-# Load our CAMulator sample dataset
-ds = xr.open_dataset('sample_data_camulator.nc')
-ds_coords = ds.assign_coords(longitude =((ds.longitude+180)%360)-180)
+ds = xr.open_dataset("sample_data_camulator.nc")
 
-europe = ds_coords['TREFHT'].isel(time=0).sel(latitude=slice(35,72), longitude=slice(-25,45))
-print (europe)
-fig, ax = plt.subplots(
-    figsize = (10,5),
-    subplot_kw= {'projection': ccrs.Robinson()})
+print ("Dimensions:", dict(ds.dims))
+print ("Coordinates:", list(ds.coords))
+print ("Global Attributes", ds.attrs)
+print ("TREFHT Attributes", ds['TREFHT'].attrs)
 
-europe.plot(
-    ax = ax,
-    transform=ccrs.PlateCarree(),
-    cmap='GnBu', 
-    cbar_kwargs={'label': 'TREFHT (K)', 
-                 'shrink': 0.7, 
-                 'orientation': 'horizontal', 
-                 'pad': 0.05}
-)
-# 4. Add coastlines and gridlines
-ax.coastlines(linewidth=0.8)
-ax.gridlines(draw_labels=True)
-plt.show()
+region = ds['TREFHT'].sel(
+    latitude=(slice(30,50)), 
+    longitude=(slice(240,270))
+    )
+print (region.shape)
