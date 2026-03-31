458b7092d93c89de0725abdee616324209e654b3
clba4780 <clba4780@colorado.edu>
2026-03-31 10:53:31 -0600

    mapping spatial mean

--- lab_07.py
+++ lab_07.py
@@ -7,13 +7,28 @@ import matplotlib.pyplot as plt
 
 ds = xr.open_dataset("sample_data_camulator.nc")
 
+# Explore structure
 print ("Dimensions:", dict(ds.dims))
 print ("Coordinates:", list(ds.coords))
 print ("Global Attributes", ds.attrs)
 print ("TREFHT Attributes", ds['TREFHT'].attrs)
 
+# Select a region and average over the latitude and longitude - one value per time step
 region = ds['TREFHT'].sel(
     latitude=(slice(30,50)), 
     longitude=(slice(240,270))
     )
-print (region.shape)
+print ("Western-US Region shape: ", region.shape)
+print (region)
+
+spatial_mean = ds['TREFHT'].mean(dim=['latitude','longitude'])
+print (f"Shape: {spatial_mean.shape}")
+print (spatial_mean.values)
+# plot time series
+ds["TREFHT"].isel(time=0).plot(cmap='plasma')
+plt.title('Flat lat/lon plot')
+plt.tight_layout
+plt.show()
+# compute the anomoly
+
+# make a robinson projection map
